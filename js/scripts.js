359e7cea0299b1874f38ae7c112574e2b620650c
kristaniabullu19 <kristaniabullu@gmail.com>
2025-04-15 18:27:51 +0800

    Menambahkan file CSS dan JS, mengubah index.html, dan menghapus beberapa file lama

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+
+
+});
